34c25309c5e7b9f1c06a4b7081e405d5ab7a62a2
Luis Jimenez <LJmnz27@live.com>
2019-02-11 21:55:50 -0500

    updates

--- app/src/main/java/org/pursuit/jsonfromscratch/StringManipulator.java
+++ app/src/main/java/org/pursuit/jsonfromscratch/StringManipulator.java
@@ -4,27 +4,55 @@ public class StringManipulator {
 
     private static StringManipulator stringManipulator;
 
-    private static StringManipulator getInstance(){
-        if(stringManipulator==null) stringManipulator = new StringManipulator();
+    private static StringManipulator getInstance() {
+        if (stringManipulator == null) stringManipulator = new StringManipulator();
         return stringManipulator;
     }
-    private StringManipulator(){}
 
-    public boolean isLessThan10(String str){
-        if (str.length() <= 9){
+    private StringManipulator() {
+    }
+
+    public boolean isLessThan10(String str) {
+
+        try {
+            if (str.length() < 9) {
+                return true;
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean isOddLength(String str) {
+        if (str.length() % 2 == 1) {
             return true;
         }
         return false;
     }
-    public boolean isOddLength(String str){
-        if(str.length() % 2 == 1){
-            return true;
-        }
-        return false;
-    }
-    public String zipperWords(String str1, String str2){
 
+    public String zipperWords(String first, String second) {
+
+        String newString = "";
+        int i = 0;
+        while (i < first.length() && i < second.length()) {
+            newString += first.charAt(i) + "" + second.charAt(i);
+            i++;
+        }
+        while (i < first.length()) {
+            newString += first.charAt(i);
+            i++;
+        }
+        while (i < second.length()) {
+            newString += second.charAt(i);
+            i++;
+        }
+        return newString;
     }
+
+
+
 }
+
 
 
--- app/src/test/java/org/pursuit/jsonfromscratch/StringManipulatorUnitTest.java
+++ app/src/test/java/org/pursuit/jsonfromscratch/StringManipulatorUnitTest.java
@@ -1,6 +1,8 @@
 package org.pursuit.jsonfromscratch;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class StringManipulatorUnitTest {
     private StringManipulator stringManipulator;
@@ -8,5 +10,9 @@ public class StringManipulatorUnitTest {
     @Before
     public void setStringManipulator(){}
 
-
+    @Test
+    public void isLessThan10(){
+    String str = "aaa";
+    Assert.assertTrue(stringManipulator.isLessThan10(str));
+    }
 }
